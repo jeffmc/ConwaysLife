9d9b0e1e22020608be1337eaa16eccd44a2ea82a
jeff <10823453+jeffmc@users.noreply.github.com>
2021-11-02 12:16:44 -0700

    Initial commit

---
+++ src/mcmillan/jeff/conwayslife/Grid.java
@@ -0,0 +1,33 @@
+package mcmillan.jeff.conwayslife;
+
+import java.util.Random;
+
+public class Grid {
+	public boolean[][] field;
+	private int width, height;
+	
+	public Grid(int w, int h) {
+		Random rand = new Random();
+		width = w;
+		height = h;
+		field = new boolean[width][height];
+		for (int y=0;y<height;y++) {
+			for (int x=0;x<width;x++) {
+				field[x][y] = rand.nextBoolean();
+			}
+		}
+	}
+	
+	public void print() {
+		System.out.println("/-------------------------\\");
+		for (int y=0;y<height;y++) {
+			System.out.print("|");
+			for (int x=0;x<width;x++) {
+				System.out.print(field[x][y]?"#":" ");;
+			}
+			System.out.print("|\n");
+		}
+		System.out.println("\\-------------------------/");
+	}
+	
+}
---
+++ src/mcmillan/jeff/conwayslife/Main.java
@@ -0,0 +1,8 @@
+package mcmillan.jeff.conwayslife;
+
+public class Main {
+	public static void main(String args[]) {
+		Grid game = new Grid(25,25);
+		game.print();
+	}
+}
