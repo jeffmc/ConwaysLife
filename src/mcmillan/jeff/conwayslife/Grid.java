0740f53904cf5a4c15eab11ae3819dece59baff1
jeff <10823453+jeffmc@users.noreply.github.com>
2021-11-10 12:06:55 -0800

    Implemented logic for game stepping
    Now to GUI!

--- src/mcmillan/jeff/conwayslife/Grid.java
+++ src/mcmillan/jeff/conwayslife/Grid.java
@@ -18,6 +18,52 @@ public class Grid {
 		}
 	}
 	
+	public void stepField() {
+		boolean[][] nextField = new boolean[width][height];
+		for (int y=0;y<height;y++) {
+			for (int x=0;x<width;x++) {
+				nextField[x][y] = stepCell(x,y);
+			}
+		}
+		field = nextField;
+	}
+	
+	 private boolean stepCell(int x, int y) { // Returns boolean for state of this cell in next frame.
+		    // If alive
+		    //   0 or 1 neighbors - dies of loneliness
+		    //   2 or 3 neighbors - survives to next round
+		    //   4 or more neighbors - dies of overcrowding
+		    // If dead
+		    //   3 neighbors - becomes populated
+		 
+		    boolean xp = x < width - 1,
+		      xn = x > 0; // x +/- available
+		    boolean yp = y < height - 1,
+		      yn = y > 0; // y +/- available
+		    int neighbors = 0;
+		    if (xp && yp)
+		      neighbors += field[x + 1][y + 1] ? 1 : 0;
+		    if (xn && yp)
+		      neighbors += field[x - 1][y + 1] ? 1 : 0;
+		    if (xp && yn)
+		      neighbors += field[x + 1][y - 1] ? 1 : 0;
+		    if (xn && yn)
+		      neighbors += field[x - 1][y - 1] ? 1 : 0;
+		    if (xp) neighbors += field[x + 1][y] ? 1 : 0;
+		    if (xn) neighbors += field[x - 1][y] ? 1 : 0;
+		    if (yp) neighbors += field[x][y + 1] ? 1 : 0;
+		    if (yn) neighbors += field[x][y - 1] ? 1 : 0;
+		    boolean nextState = false;
+		    if (field[x][y]) {
+		      if (neighbors >= 2 && neighbors <= 3) {
+		        nextState = true;
+		      }
+		    } else {
+		      if (neighbors == 3) nextState = true;
+		    }
+		    return nextState;
+		  };
+	
 	public void print() {
 		System.out.print("\n/-------------------------\\\n");
 		for (int y=0;y<height;y++) {
--- src/mcmillan/jeff/conwayslife/Main.java
+++ src/mcmillan/jeff/conwayslife/Main.java
@@ -1,8 +1,19 @@
 package mcmillan.jeff.conwayslife;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String args[]) {
 		Grid game = new Grid(25,25);
 		game.print();
+		Scanner console = new Scanner(System.in);
+		while (true) { // TODO: Add GUI functionality.
+			if (console.nextLine().trim().toLowerCase().length()>3) break;
+			game.stepField();
+			game.print();
+		}
+		System.out.println("Exited!");
+		console.close();
+		System.exit(0);
 	}
 }
